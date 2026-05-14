1e1b5e218240329ef587a696d192428b36c36df2
tumusaisrisindhu <saisrisindhu.tumu@gmail.com>
2026-05-14 21:01:06 +0530

    Added backend of compound interest and tenure option to simple interest in financial calculators

--- modules/calculator/financial/simple_interest.py
+++ modules/calculator/financial/simple_interest.py
@@ -1,7 +1,32 @@
+def convert_to_years(time, tenure_type):
+    time = float(time)
+
+    conversions = {
+        "days": time / 365,
+        "months": time / 12,
+        "quarterly": time / 4,
+        "half-yearly": time / 2,
+        "yearly": time
+    }
+
+    return conversions.get(
+        tenure_type,
+        time
+    )
+
 def calculate_simple_interest(data):
-    principal = float(data["principal"])
-    rate = float(data["rate"])
-    time = float(data["time"])
+    principal = float(
+        data["principal"]
+    )
+
+    rate = float(
+        data["rate"]
+    )
+
+    time = convert_to_years(
+        data["time"],
+        data["tenure_type"]
+    )
 
     simple_interest = (
         principal * rate * time
